3209980a552b142c8d3ce9290c05010c7690d491
ChristianAlfonsoDev <alfonso.christian.macatangay@gmail.com>
2026-09-10 20:27:07 +0800

    async await

--- main/scripts/async-await.js
+++ main/scripts/async-await.js
@@ -1,5 +1,45 @@
-async function test() {
-    return "hello world";
-}
+let userInput = document.querySelector("#input");
+let btn = document.querySelector("#btn");
 
-test().then(result => console.log(result));
+
+btn.addEventListener("click",()=> {
+    // console.log("you clicked the button")
+    let userInputValue = userInput.value;
+
+    if(userInputValue === "") {
+        console.log("Please enter a value");
+        return;
+    }
+
+    function getData(){
+        return new Promise((resolve,reject)=>{
+
+            setTimeout(()=> {
+
+                if(userInputValue === "Lyca") {
+                    resolve("You are a lalabs user");
+                } else {
+                    reject("You are not a lalabs user");
+                }
+            },2000)
+
+        })
+    }
+
+
+    async function getUserData(){
+        try {
+            let result = await getData();
+            console.log(result);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    getUserData();
+    
+
+
+
+}) 
+
